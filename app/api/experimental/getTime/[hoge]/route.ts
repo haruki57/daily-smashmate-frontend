5b5ca8feede245e22961b0ae72beb600a5a3a584
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-07-06 15:27:14 +0900

    Remove unsed things

--- app/api/experimental/getTime/[hoge]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(request: Request, 
-  { params }: { params: any }) {
-  return NextResponse.json({
-    timestamp: Date.now(),
-    params: JSON.stringify(params|| {}),
-  });
-}
