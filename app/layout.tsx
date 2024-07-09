780da4ae4102cc6d881813c4e997c1870ebc6ef2
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-07-09 13:35:06 +0900

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 import { RootHeader } from './_components/RootHeader';
 import { RootFooter } from './_components/RootFooter';
+import { GoogleAnalytics } from '@next/third-parties/google';
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export const metadata: Metadata = {
   title: {
@@ -30,6 +33,7 @@ export default function RootLayout({
         <div className="container mx-auto w-screen max-w-4xl">{children}</div>
         <RootFooter />
       </body>
+      {GA_ID && <GoogleAnalytics gaId={GA_ID} />}
     </html>
   );
 }
