301176c6da832738c99aaff4e13c358892ea2078
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-07-12 14:11:41 +0900

    metadataBase

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,11 @@ export const metadata: Metadata = {
   },
   description:
     'デイリースマメイトは、スマメイト27期以降の戦績を閲覧できるサービスです。',
-  metadataBase: process.env.URL ? new URL(process.env.URL) : undefined,
+  metadataBase: process.env.VERCEL_URL
+    ? new URL(process.env.VERCEL_URL)
+    : process.env.URL
+    ? new URL(process.env.URL)
+    : undefined,
   formatDetection: {
     telephone: false,
   },
