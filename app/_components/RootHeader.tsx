91c20fde5ac79342d2905740a8f7d1bca5fc42be
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-07-15 15:42:27 +0900

    RootHeader Rayout

--- app/_components/RootHeader.tsx
+++ app/_components/RootHeader.tsx
@@ -9,59 +9,61 @@ export function RootHeader() {
       <div className="flex h-10 w-full items-center justify-between bg-slate-900 text-slate-100">
         <Link
           href="/"
-          className="ml-4 flex gap-1 text-xl font-bold"
+          className="ml-4 flex items-center justify-start gap-1 font-bold md:text-xl"
           style={{
             fontFamily: "'Arial', sans-serif",
             textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)' /* 文字の影 */,
           }}
         >
-          <svg
-            viewBox="0 0 480 480"
-            height="24px"
-            width="24px"
-            xmlns="http://www.w3.org/2000/svg"
-            version="1.1"
-          >
-            <g>
-              <rect
-                fill="#000000"
-                height="322"
-                id="svg_4"
-                rx="8"
-                ry="8"
-                stroke="#ffffff"
-                stroke-width="27"
-                width="100"
-                x="40"
-                y="136"
-              />
-              <rect
-                fill="#000000"
-                height="398"
-                id="svg_5"
-                rx="8"
-                ry="8"
-                stroke="#ffffff"
-                stroke-width="27"
-                transform="matrix(1 0 0 1 0 0)"
-                width="100"
-                x="192"
-                y="60"
-              />
-              <rect
-                fill="#000000"
-                height="236"
-                id="svg_6"
-                rx="8"
-                ry="8"
-                stroke="#ffffff"
-                stroke-width="27"
-                width="100"
-                x="342"
-                y="220"
-              />
-            </g>
-          </svg>
+          <div className="mt-[-4px] h-5 md:h-6">
+            <svg
+              viewBox="0 0 480 480"
+              height="full"
+              width="full"
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1"
+            >
+              <g>
+                <rect
+                  fill="#000000"
+                  height="322"
+                  id="svg_4"
+                  rx="8"
+                  ry="8"
+                  stroke="#ffffff"
+                  stroke-width="27"
+                  width="100"
+                  x="40"
+                  y="136"
+                />
+                <rect
+                  fill="#000000"
+                  height="398"
+                  id="svg_5"
+                  rx="8"
+                  ry="8"
+                  stroke="#ffffff"
+                  stroke-width="27"
+                  transform="matrix(1 0 0 1 0 0)"
+                  width="100"
+                  x="192"
+                  y="60"
+                />
+                <rect
+                  fill="#000000"
+                  height="236"
+                  id="svg_6"
+                  rx="8"
+                  ry="8"
+                  stroke="#ffffff"
+                  stroke-width="27"
+                  width="100"
+                  x="342"
+                  y="220"
+                />
+              </g>
+            </svg>
+          </div>
           デイリースマメイト
         </Link>
 
