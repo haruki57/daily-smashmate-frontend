b8ca0aff70ae193b921f48a2017cb5575b7a7bee
Haruki Nagasawa <hnagasawa57@gmail.com>
2026-04-05 12:28:57 +0900

    perf: /_next/imageとstatic assetsのキャッシュヘッダーを追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/_next/image',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, s-maxage=604800, stale-while-revalidate=86400',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
